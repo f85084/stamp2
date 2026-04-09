9878a6855e3298a381b8aea3e27c534cdcfbc4ec
Anna <anna22.chen@ehsn.com.tw>
2026-04-09 16:49:37 +0800

    fix: 將 config.js 加入版本控制以修正線上空白問題

---
+++ public/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCB1ubZJQEe4h_IlZYhE7kFTEsBaqWY-4Q",
+  authDomain: "stamp-f8d15.firebaseapp.com",
+  projectId: "stamp-f8d15",
+  messagingSenderId: "290709470994",
+  appId: "1:290709470994:web:5c59b500f9cda459a37dcb",
+  measurementId: "G-ZP50EPTXC8",
+};
+
+// 管理員密碼（用於「集點 / 兌換」驗證）
+export const adminPassword = "admin";
